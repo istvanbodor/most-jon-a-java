b7f4f28af3bb1c85198acc6687ddab90fc27bc01
Bodor István <istvanbodor19@gmail.com>
2025-03-24 00:21:31 +0100

    +javadoc, modul takarítás

--- src/main/java/lab/proj/mostjonajava/utils/HivasLogger.java
+++ src/main/java/lab/proj/mostjonajava/utils/HivasLogger.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 public class HivasLogger {
 
+    /**
+     * A tesztelést segítő logger függvény - kiírja a hívott függvényt és annak paramétereit
+     * @param hivottFuggveny a hívott függvény
+     * @param parameterek a hívott függvény paraméterei
+     */
     public static void hivasLogolasa(String hivottFuggveny, ArrayList<String> parameterek) {
         System.out.println(hivottFuggveny + "fut a következő paraméterekkel: " + parameterek);
     }
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module lab.proj.mostjonajava {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires lombok;
-
-
-    opens lab.proj.mostjonajava to javafx.fxml;
-    exports lab.proj.mostjonajava;
-}
