f9f66e15ef0d7078d33562355fd5d9e2dda49c32
dobondireisz <hannadobondireisz22@gmail.com>
2025-03-24 05:07:07 +0100

    rovar_kommentezese

--- src/main/java/lab/proj/mostjonajava/model/Rovar.java
+++ src/main/java/lab/proj/mostjonajava/model/Rovar.java
@@ -19,6 +19,10 @@ public class Rovar extends Jatekos {
     private boolean vagoKepesseg;
     private boolean benulas;
 
+    /**
+     * Rovar konstruktora.
+     * @param nev
+     */
     public Rovar(String nev) {
         super(nev);
         hivasLog("Rovar(String nev)", List.of("nev: String - " + nev), 1);
@@ -30,34 +34,70 @@ public class Rovar extends Jatekos {
         log("Rovar letrejott - ROVAR_ID: " + id);
     }
 
+    /**
+     * Kivalaszt egy tektont.
+     * @return
+     */
     public Tekton tektonKivalasztasa() {
         return null;
     }
 
+    /**
+     * Noveli a jatekos pontszamat a megadott ertekkel.
+     * @param ertek
+     */
     public void pontNovelese(int ertek) {
     }
 
+    /**
+     * A rovar lepeseert felelos egyik tektonrol a masikra.
+     * @param tekton
+     */
     public void lepes(Tekton tekton) {
     }
 
+    /**
+     * Spora elfogyasztasa.
+     * @param tekton
+     */
     public void sporaElfogyasztas(Tekton tekton) {
     }
 
+    /**
+     * A rovar lepes szamat csokkenti, azaz hanyat léphet.
+     */
     public void lepesSzamNoveles() {
     }
 
+    /**
+     * A rovar lepes szamat noveli, azaz hanyat léphet.
+     */
     public void lepesSzamCsokkentes() {
     }
 
+    /**
+     * Nem engedi, hogy a rovar vagjon.
+     */
     public void vagoKepessegTiltas() {
     }
 
+    /**
+     * Ellenorzi, hogy a rovar benult allapotban van-e.
+     */
     public void benulasEllenorzes() {
     }
 
+    /**
+     * Ellenorzi, hogy a rovar tud-e vagni.
+     */
     public void vagasEllenorzes() {
     }
 
+    /**
+     * Elvagja a fonalat a kijelolt tekton es az a tekton kozott,
+     * ahol a rovar all.
+     * @param tekton
+     */
     public void fonalVagas(Tekton tekton) {
     }
 }
