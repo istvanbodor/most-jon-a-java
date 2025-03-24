87a7725a36f541418688ef7a27dd3c29850f5038
Mátyás Vivien <matyasvivi2005@gmail.com>
2025-03-24 05:12:08 +0100

    Tekton osztály kommentelése.

--- src/main/java/lab/proj/mostjonajava/model/Tekton.java
+++ src/main/java/lab/proj/mostjonajava/model/Tekton.java
@@ -26,24 +26,89 @@ public abstract class Tekton {
     private int sporaSzam;
     private int fonalakElettartama;
 
+
+    /**
+     * Konstruktor felülírása.
+     */
     public Tekton() {
         id = nextId++;
         log("Tekton létrejött - TEKTON_ID: " + id);
     }
 
+    /**
+     * Tekton konstruktor.
+     * @return
+     */
     public abstract Tekton ujTektonLetrehozasa();
+
+    /**
+     * Egy tekton ketté törik.
+     */
     public abstract void ketteTores();
+
+    /**
+     * Jelzi, hogy van-e fonal a két tekton között (amin áll és ami a paraméter).
+     * @param tekton
+     * @return
+     */
     public boolean vanFonalKozottuk(Tekton tekton) { return false; }
+
+    /**
+     * Hozzáad egy fonalat a tektonhoz.
+     * @param fonal
+     */
     public void fonalHozzaadasa(GombaFonal fonal) {}
+
+    /**
+     * Törlődik a megadott fonal.
+     * @param fonal
+     */
     public void fonalTorlese(GombaFonal fonal) {}
+
+    /**
+     * Minden fonal elszakad, ami az adott tektonhoz tertozott.
+     */
     public void mindenFonalElszakadasa() {}
+
+    /**
+     * Hozzáad egy spórát.
+     * @param spora
+     */
     public void sporaHozzaadasa(Spora spora) {}
+
+    /**
+     * Töröl egy spórát.
+     * @param spora
+     */
     public void sporaTorlese(Spora spora) {}
+
+    /**
+     * Bizonyos tektonoknál (pl. Eltűnő fonalas) számolni kell az eltelt időt, ezt valósítja meg ez a fügvény.
+     */
     public void eletIdoCsokkentes() {}
+
+    /**
+     * Ellenörzi, hogy növeszthető-e a tektonon gombafonal.
+     * @return
+     */
     public boolean gombafonalNoveszthetoE() { return false; }
+
+    /**
+     * Ellenörzi, hogy növeszthető-e gombatest az adott tekton.
+     * @return
+     */
     public boolean gombatestNoveszthetoE() { return false; }
+
+    /**
+     * Áthelyezi a rovart a megadott tektonra.
+     * @param tekton
+     */
     public void rovarMozgasa(Tekton tekton) {}
+
+    /**
+     * Leellenörzi, hogy önmaga-e.
+     * @param tekton
+     * @return
+     */
     public boolean egyenlo(Tekton tekton) { return false; }
-    public int getSporaSzam() { return sporaSzam; }
-    public int getFonalElettartama() { return fonalakElettartama; }
 }
