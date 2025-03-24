77d88f6b60f2d1620e005d77a5ff74835684dfc6
Bálint Dénes <denesbalint08@gmail.com>
2025-03-24 06:54:29 +0100

    FejlettSporaSzoras implementalasa

--- src/main/java/lab/proj/mostjonajava/FungoriumApplication.java
+++ src/main/java/lab/proj/mostjonajava/FungoriumApplication.java
@@ -11,9 +11,11 @@ import lab.proj.mostjonajava.model.Gombatest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 import static lab.proj.mostjonajava.utils.Logger.hibaLog;
+import static lab.proj.mostjonajava.utils.Logger.log;
 import static lab.proj.mostjonajava.utils.Parancsok.*;
 
 public class FungoriumApplication extends Application {
@@ -120,11 +122,49 @@ public class FungoriumApplication extends Application {
     }
 
     private static void testSimaSporaSzoras(String[] parameterek) {
-        //todo teszteset
+        Tekton tekton = new Tekton() {
+            @Override
+            public Tekton ujTektonLetrehozasa() {
+                return null;
+            }
+
+            @Override
+            public void ketteTores() {
+
+            }
+        };
+        SimaSpora sspora = new SimaSpora();
+        tekton.sporaHozzaadasa(sspora);
     }
 
     private static void testFejlettSporaSzoras(String[] parameterek) {
-        //todo teszteset
+        Tekton tekton1 = new Tekton() {@Override public Tekton ujTektonLetrehozasa() {
+                return null;
+            }@Override public void ketteTores() {}
+        };
+        Tekton tekton2 = new Tekton() {@Override public Tekton ujTektonLetrehozasa() {
+                return null;
+            }@Override public void ketteTores() { }
+        };
+        Tekton tekton3 = new Tekton() {@Override public Tekton ujTektonLetrehozasa() {
+                return null;
+            } @Override public void ketteTores() {}
+        };
+        List<Tekton> tektonok1 = new ArrayList<>();
+        tektonok1.add(tekton2);
+        List<Tekton> tektonok2 = new ArrayList<>();
+        tektonok2.add(tekton1);
+        tektonok2.add(tekton3);
+        List<Tekton> tektonok3 = new ArrayList<>();
+        tektonok3.add(tekton2);
+        tekton1.setSzomszedosTektonok(tektonok1);
+        tekton2.setSzomszedosTektonok(tektonok2);
+        tekton3.setSzomszedosTektonok(tektonok3);
+        Gombatest gombatest = new Gombatest();
+        gombatest.setKilohetoSporakSzama(10);
+        tekton1.setGombatest(gombatest);
+        gombatest.sporaKiloves(tekton3);
+        log("FejlettSporaSzoras");
     }
 
     private static void testGombaTestNovesztes(String[] parameterek) {
